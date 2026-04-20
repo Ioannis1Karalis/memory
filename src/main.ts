bb51f7a721e9ead16f14994802d6848c7311cdc2
Ioannis Karalis <i.karalis@outlook.de>
2026-04-20 22:55:36 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import './styles/main.scss'
+
+init()
+
+function init(){
+    const fieldRef = document.getElementById("field")
+    if(fieldRef){
+        fieldRef.addEventListener("click", e =>{
+            const card = (e.target as HTMLElement).closest(".card") as HTMLButtonElement
+            if(card){
+                card.classList.toggle("is-flipped")
+            }
+        })
+    }
+}
+
